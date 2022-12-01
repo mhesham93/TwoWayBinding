f4cb7ce6ad47abef1c014f7ee751aa6f5f5bded1
Mohamed <mhesham007@hotmail.com>
2022-11-30 19:03:52 -0600

    Initial Commit

---
+++ TwoWayBinding/TwoWayBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingApp.swift
+//  TwoWayBinding
+//
+//  Created by Mohamed Said on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
